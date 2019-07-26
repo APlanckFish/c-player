f333e3652e545349a70fe019a690369059ec2121
APlanckFish <327987913@qq.com>
2019-07-26 14:53:46 +0800

    Updates

---
+++ precache-manifest.6ae18eaf521f951f3f7395360fe54dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ccf7ae61bf4efd8b06eb2c75f250df",
+    "url": "/c-player/index.html"
+  },
+  {
+    "revision": "9499399440d0a33207f7",
+    "url": "/c-player/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "481bd080b0abda9f04ea",
+    "url": "/c-player/static/css/main.92b268e7.chunk.css"
+  },
+  {
+    "revision": "9499399440d0a33207f7",
+    "url": "/c-player/static/js/2.e0a2cc07.chunk.js"
+  },
+  {
+    "revision": "481bd080b0abda9f04ea",
+    "url": "/c-player/static/js/main.d95d29c8.chunk.js"
+  },
+  {
+    "revision": "2cae03447375c615e59a",
+    "url": "/c-player/static/js/runtime~main.84e80af9.js"
+  },
+  {
+    "revision": "f85ea331efd07d2cd8cf9e7fd9e9b5c2",
+    "url": "/c-player/static/media/video2.f85ea331.mp4"
+  }
+]);
